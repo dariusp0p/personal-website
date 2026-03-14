2fa472233ab77b75b97435ac55bb8eae59f26d41
dariusp0p <dariuspop2005@gmail.com>
2026-03-14 19:08:32 +0200

    feat: finnished

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,7 @@ import { FloatingNav } from "@/components/ui/sections/FloatingNavbar";
 import ProjectSearchEngine from "@/components/ui/ProjectSearchEngine";
 import ProjectCard from "@/components/ui/ProjectCard";
 import Footer from "@/components/ui/sections/Footer";
+import AboutMeCard from "@/components/ui/AboutMeCard";
 
 const navItems = [
   { name: "Home", link: "/" },
@@ -18,7 +19,13 @@ export default function ProjectsPage() {
     <>
       <FloatingNav navItems={navItems} />
       <main className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold mb-8 text-center">Coming Soon!</h1>
+        <AboutMeCard
+          paragraphs={[
+            "Second-year CS student at UBB Cluj-Napoca with early experience in web development and software engineering. I’ve built and deployed both web and desktop applications, taking them from idea to production.",
+            "I’m currently exploring Cloud Infrastructure, DevOps, and AI/ML. I’m looking for an internship where I can contribute meaningful work while learning as much as possible from experienced engineers.",
+            "Outside of tech, I stay active through music and martial arts. As Music Director in my local church band, I play piano and coordinate live performances, which has strengthened my discipline, teamwork, and focus under pressure. I have also practiced Muay Thai and Karate, experiences that taught me resilience, self-discipline, and perseverance, qualities I carry into both personal and professional challenges.",
+          ]}
+        />
       </main>
       <Footer />
     </>
